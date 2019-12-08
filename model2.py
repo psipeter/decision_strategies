5fafeaa4284765d6c5fa02e2a53f0a134f484943
Peter Duggins <psipeter@gmail.com>
2019-12-08 11:11:33 -0500

    model2 has no time cells but can reproduce more extreme ncues distributions

--- model2.py
+++ model2.py
@@ -39,8 +39,10 @@ def ncues(data, dt=0.001):
     for ncues in range(6):
         data_range = data[int(T*ncues/dt):int(T*(ncues+1)/dt)]
         # if decision has been A or B for more than half of the time in this slot, model has decided
-        if (len(np.where(data_range[:,2] < data_range[:,0])[0]) > len(data_range)/2 or
-                len(np.where(data_range[:,2] < data_range[:,1])[0]) > len(data_range)/2):
+#         if (len(np.where(data_range[:,2] < data_range[:,0])[0]) > len(data_range)/2 or
+#                 len(np.where(data_range[:,2] < data_range[:,1])[0]) > len(data_range)/2):
+        # if decision is A or B at the end of this time slot, model has decided
+        if data_range[:,0][-1] > data_range[:,2][-1] or data_range[:,1][-1] > data_range[:,2][-1]:
             return ncues+1
     return 6
 
@@ -84,12 +86,19 @@ def get_ncues_oracle(trial):
         if evidence_opt[n][choice_opt] > evidence_opt[-1][choice_opt-1]:
             return n+1
 
+# def get_ncues_greedy(trial):
+#     # the first time at which the evidence accumulated for the winning option outweights the evidence accumulated for the non-winner
+#     # (given the actual cue values on this trial)
+#     evidence_opt, choice_opt = get_evidence_opt(trial)
+#     for n in range(6):
+#         if evidence_opt[n][choice_opt] > evidence_opt[n][choice_opt-1]:
+#             return n+1
+        
 def get_ncues_greedy(trial):
-    # the first time at which the evidence accumulated for the winning option outweights the evidence accumulated for the non-winner
-    # (given the actual cue values on this trial)
+    # the first time at which the evidence accumulated for one option outweights the evidence accumulated for the other option
     evidence_opt, choice_opt = get_evidence_opt(trial)
     for n in range(6):
-        if evidence_opt[n][choice_opt] > evidence_opt[n][choice_opt-1]:
+        if evidence_opt[n][0] != evidence_opt[n][1]:
             return n+1
 
 
@@ -97,13 +106,11 @@ def get_ncues_greedy(trial):
 weights = np.array([0.706, 0.688, 0.667, 0.647, 0.625, 0.6]) # weights of cues (from experiment)
 T = 1.0  # time interval (s) for which the choices are presented
 
-def run_model(trial, gain, default):
+def run_model(trial, gain_in, default_in):
 
     values_A, values_B = read_values(trial)
     values = [[values_A[n], values_B[n]] for n in range(len(values_A))]
 
-    gain_process = nengo.processes.PresentInput([[gain]], presentation_time=6.0)
-    default_process = nengo.processes.PresentInput([[default]], presentation_time=6.0)
     weight_process = nengo.processes.PresentInput(weights, presentation_time=T)
     value_process = nengo.processes.PresentInput(values, presentation_time=T)
 
@@ -112,14 +119,14 @@ def run_model(trial, gain, default):
         # Inputs
         weight_inpt = nengo.Node(weight_process)
         value_inpt = nengo.Node(value_process)
-        gain_inpt = nengo.Node(gain_process)
-        default_inpt = nengo.Node(default_process)
+        gain_inpt = nengo.Node(gain_in)
+        default_inpt = nengo.Node(default_in)
         weighted_value_node = nengo.Ensemble(1, 3, neuron_type=nengo.Direct())
         evidence_node = nengo.Ensemble(1, 2, neuron_type=nengo.Direct())
         
         # Ensembles
-        gain = nengo.Ensemble(1000, 1, radius=2, label="LC")  # represents emotional modulation of weights
-        default = nengo.Ensemble(1000, 1, radius=3)
+        gain = nengo.Ensemble(200, 1, radius=2, label="LC")  # represents emotional modulation of weights
+        default = nengo.Ensemble(200, 1, radius=3)
         multiply = nengo.Ensemble(4000, 3, radius=4, label="dlPFC")  # represents values dim=[0,1] and weight dim=[2]
         evidence = nengo.Ensemble(4000, 2, radius=4)  # 2D integrator accumulates weighted evidence
         utility = nengo.Ensemble(4000, 3, radius=4, label="pre-SMA")  # inputs to BG
@@ -156,7 +163,7 @@ def run_model(trial, gain, default):
         p_multiply = nengo.Probe(multiply, synapse=0.1)
         p_evidence = nengo.Probe(evidence, synapse=0.1)
         p_utility = nengo.Probe(utility, synapse=0.1)
-        p_decision = nengo.Probe(decision.output, synapse=0.1)  
+        p_decision = nengo.Probe(decision.output, synapse=0.1)
         p_evidence_node = nengo.Probe(evidence_node, synapse=0.1)
 
     sim = nengo.Simulator(model, seed=0, progress_bar=True)
@@ -181,14 +188,14 @@ def run_trials(
     n_trials_train=48,
     n_trials_test=48,
     delta_gain=1e-1,
-    delta_default=1e-1,
+    delta_default=3e-1,
     strategy=1,  # 0 favors speed, 1 favors accuracy
     rng=np.random.RandomState(seed=0),
     plot=False):
 
     # Training
-    gain = 1
-    default = 0.1
+    gain = 0
+    default = 0.5
     trial_list = np.arange(48)
     rng.shuffle(trial_list)
     for trial in range(n_trials_train):
@@ -204,13 +211,9 @@ def run_trials(
         correct = is_correct(data['decision'], trial)
         print('correct:', correct==1)
         print('ncues_model:', ncues_model)
+        print('ncues_opt', ncues_opt)
+        print('ncues_greedy', ncues_greedy)
         print('ncues_target', ncues_target)
-        if correct and ncues_model > ncues_target:  # correct and late
-            gain += delta_gain * (1-strategy)
-            default -= delta_default * (1-strategy)
-        if not correct and ncues_model < ncues_target:  # incorrect and early
-            gain -= delta_gain * strategy
-            default += delta_default * strategy
 
         if plot:
             fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=((12, 12)))
@@ -226,17 +229,24 @@ def run_trials(
             ax2.axvline(x=ncues_opt, alpha=0.5, label='ncues_opt', color='b')
             ax2.axvline(x=ncues_greedy, alpha=0.5, label='ncues_greedy', color='m')
             ax2.axvline(x=ncues_target, alpha=0.5, label='ncues_target', color='k')
-            ax1.set(xticks=([0, 1, 2, 3, 4, 5, 6]), ylim=((0, 3.5)), ylabel='utility', title='trial %s, correct=%s'%(trial, correct))  # ylim=((0, np.sum(weights))),
+            ax1.set(xticks=([0, 1, 2, 3, 4, 5, 6]), ylim=((0, 3.5)), ylabel='utility', title='trial %s, gain=%.3f, default=%.3f, correct=%s'%(trial, gain, default, correct))  # ylim=((0, np.sum(weights))),
             ax2.set(xticks=([0, 1, 2, 3, 4, 5, 6]), ylabel='BG values')
             ax1.legend(loc='upper left')
             ax2.legend(loc='upper left')
             plt.savefig("plots/timeseries_train_%s.png"%trial)
             plt.close()
 
-        # update weights on learned connection for next trial
+        # update values for next trial
+        if correct and ncues_model > ncues_target:  # correct and late
+            gain += delta_gain * (1-strategy) * rng.uniform(0, 1)
+            default -= delta_default * (1-strategy) * rng.uniform(0, 1)
+        if not correct and ncues_model < ncues_target:  # incorrect and early
+            gain -= delta_gain * strategy * rng.uniform(0, 1)
+            default += delta_default * strategy * rng.uniform(0, 1)
         np.savez("data.npz", gain=gain, default=default, strategy=strategy, participant=participant, delta_gain=delta_gain, delta_default=delta_default)
 
     # Testing
+    print("final: gain=%.3f, default=%.3f"%(gain, default))
     corrects_simulated = np.zeros((n_trials_test, 1))
     corrects_empirical = np.zeros((n_trials_test, 1))
     ncues_simulated = np.zeros((n_trials_test, 1))
@@ -272,7 +282,7 @@ def run_trials(
             ax2.set(xticks=([0, 1, 2, 3, 4, 5, 6]), ylabel='BG values')
             ax1.legend(loc='upper left')
             ax2.legend(loc='upper left')
-            plt.savefig("plots/timeseries_train_%s.png"%trial)
+            plt.savefig("plots/timeseries_test_%s.png"%trial)
             plt.close()
 
         corrects_simulated[trial] = is_correct(data['decision'], trial)
@@ -395,6 +405,5 @@ def plot_participant_data():
 
 # plot_participant_data()
 
-run_trials(participant=0, plot=True, n_trials_train=3, n_trials_test=4, strategy=0)
-
-# run_trials(participant=11, plot=True, incorrect_early=1.0, correct_late=-0.3)
+run_trials(participant=0, plot=True, strategy=0)
+# run_trials(participant=11, plot=True, strategy=1)
